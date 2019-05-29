04b5892154bff1939f6abd63abce1d0d6818482d
Daniel Wheeler <danelhombre@gmail.com>
2019-05-28 22:42:45 -0700

    added in logic for percent text and bar

--- www/js/app.js
+++ www/js/app.js
@@ -1,21 +1,30 @@
 (function()
 {
+    // Requirements
     const remote = require('electron').remote;
     const window = remote.getCurrentWindow();
     const ioHook = require('iohook');
+    const timer  = require('timers');
+
+    // Global variables
+    var ultTimeoutObject;
+    var baseUltIncrementRate = 5; // per second.
+    var ultimateTimer        = 3000; // milliseconds
+    var percentBarColors     = ["yellow", "#ffbc40", "#e07b00", "red"]; // index 4 is 100%;
     
     function init() {
         // Set up the close button for the app.
-        document.getElementById("button-close").addEventListener("click", function (e)
-        {
-            window.close();
-        }); 
+        document.getElementById("button-close").addEventListener("click", () => { window.close(); }); 
+        
+        // Make 6 out of the initial template.
+        setupHeroes();
 
-        ioHook.on("keyup", event => {
-            handleKeyboardShortcuts(event.keycode);
-        });
-
+        // Handle all keyboard input.
+        ioHook.on("keyup", event =>  handleKeyboardShortcuts(event));
         ioHook.start();
+
+        // Star the ultimate tracking.
+        ultTimeoutObject = setInterval(ultimateIncrement, ultimateTimer);
     }; 
     
     // Check when the application is ready.
@@ -25,8 +34,46 @@
         init(); 
     };
 
-    function handleKeyboardShortcuts(keycode)
+    function setupHeroes()
     {
+        var element = $(".hero-container:eq(0)");
 
+        for(let count = 0; count < 5; count++)
+            element.after( element.clone() );
+    }
+
+    function handleKeyboardShortcuts(event)
+    {
+        var keyCode = event.keycode;
+
+        // Handle different input.
+        console.log("DEBUG: key  " + keyCode);
+    }
+
+    function ultimateIncrement()
+    {
+        // Loop through each hero to auto increment.
+        $(".hero-container").each(function()
+        {
+            var percent = parseInt($(this).data('percent') || 0);
+            percent += baseUltIncrementRate;
+            
+            if(percent > 100)
+                percent = 100;
+
+            var barColorIndex = Math.floor(percent / 33);
+            var barColor      = percentBarColors[barColorIndex];
+
+            var fontColor = "black";
+            if(percent >= 50)
+                fontColor = "white";
+
+            // Set the new percent.
+            $(this).data('percent', percent);
+            $(this).find('.hero-bar-percent-background').css('width', percent + '%');
+            $(this).find('.hero-bar-percent-background').css('background', barColor);
+            $(this).find('.hero-bar-percent-text').text(percent + '%');
+            $(this).find('.hero-bar-percent-text').css('color', fontColor);
+        });
     }
 })();
